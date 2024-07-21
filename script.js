ef207f688405dab737e5a174b3b137df4d53100c
Pratik Gudape <pratik.gudape23@pccoepune.org>
2024-07-21 21:57:37 +0530

    done with code

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let timeLimit = 60;
 let hitNum = 0;
+let hit = 0;
 let score = 0;
+
+function gameEnd() {
+    document.querySelector("#center-panel").innerHTML = `
+    <div id="game-end-box">
+        <h1 id="game-over-text"> Game over <h1/>
+        <div class="tp-cont">
+            <h3>FINAL SCORE</h3>
+            <div id="score-box" class="info-box">${score}</div>
+        </div>
+    </div>
+    `;
+}
+
 function timer() {
        let interval = setInterval(() => {
         if (timeLimit > 0) {
@@ -9,11 +23,12 @@ function timer() {
         }
         else {
             clearInterval(interval);
+            gameEnd();
         }
         }, 1000);
     
 }
-// timer();
+timer();
 
 function hitChange() {
     hitNum = Math.floor(Math.random()*10);
@@ -35,5 +50,19 @@ function createBubbles() {
     }
     document.querySelector("#center-panel").innerHTML = clutter;
 }
-
 createBubbles();
+
+function gameBrain() {
+    document.querySelector("#center-panel").addEventListener("click", (dets) => {
+        hit = Number(dets.target.textContent);
+        if (hit === hitNum) {
+            // 1. Update score by 10
+            scoreChange();
+            // 2. update the hit target
+            hitChange();
+            // 3. create new bubbles 
+            createBubbles();
+        }
+    })
+}
+gameBrain();
